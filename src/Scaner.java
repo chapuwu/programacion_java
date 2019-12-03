ae69241f3af2f0856c89b8dab4a479c7db1acbf0
chapuwu <58433204+chapuwu@users.noreply.github.com>
2019-12-03 19:27:19 -0300

    Scanner uwu

---
+++ src/Scaner.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Scaner {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Proporciona el titulo:");
+        String titulo = scanner.nextLine();
+        System.out.println("Proporciona el autor:");
+        String autor = scanner.nextLine();
+        System.out.println(titulo + " " + "fue escrito por" + " " + autor);
+    }
+
+}
